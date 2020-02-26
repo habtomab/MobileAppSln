e97d410768b695698e70081d9c5ae6ac49f8dd13
habtom gebre <hatomg127@gmail.com>
2020-02-26 14:20:30 -0600

    updated

--- app/src/main/java/com/example/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -1,12 +1,56 @@
 package com.example.dinnerdecider
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+      // array list of Foods
+
+        var foodOptions= ArrayList<String>(
+            arrayListOf(
+                "Hamburger", "Pizza",
+                "Mexican", "American", "Chinese"
+            )
+        )
+
+    //Override method
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+        }
+
+
+    //Random Access
+
+        fun randomDecision(view: View) {
+            var s=foodOptions.size
+            val randomInt = (s * Math.random()).toInt()
+            txtYourChoice.text=foodOptions.get(randomInt)
+
+        }
+
+
+
+    // addd  new food
+        fun addNewFood(view: View) {
+            var input=etAddNewFood?.text.toString().trim()
+
+            if (!input.isNullOrBlank()){
+
+                foodOptions.add(etAddNewFood.text.toString())
+                Toast.makeText(this,"New FoodType is added Successfully: "+ input, Toast.LENGTH_SHORT).show()
+                //for reset the EditText
+                etAddNewFood.text=null
+                txtYourChoice.text=input
+            }
+
+        }
+
+
     }
-}
+
